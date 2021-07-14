21317a8a49b450c8438e8f9183d7d9c1151d2d52
ilgizDashkin <48960359+ilgizDashkin@users.noreply.github.com>
2021-07-14 13:06:39 +0500

    first commit

---
+++ poverka.js
@@ -0,0 +1,34 @@
+function date_year() {
+    // ищем год протокола
+    let date_year = document.getElementById('date').value.split("-")
+    // console.log(date_year)
+    return date_year[0]
+}
+// Передвижная высоковольтная лаборатория
+let nomer_svid_lab='8/81'
+let data_svid_lab='12.11.'
+// Передвижная высоковольтная лаборатория SEBA для СПЭ
+let nomer_svid_lab_seba='8/195'
+let data_svid_lab_seba='12.11.'
+// Мегаомметр
+let nomer_svid_meg='8/1507'
+let data_svid_meg='27.10.'
+// Психрометр
+let nomer_svid_psih='9/3695'
+let data_svid_psih='28.10.'
+// барометр
+let nomer_svid_bar='9/3120'
+let data_svid_bar='26.10.'
+// термометр
+let nomer_svid_term='9/1320'
+let data_svid_term='26.10.'
+// ПФИ
+let nomer_svid_pfi='8/5972'
+let data_svid_pfi='12.11.'
+// коэфиц-3
+let nomer_svid_ktr='8/5992'
+let data_svid_ktr='12.11.'
+// тангенс
+let nomer_svid_tg='8/4740'
+let data_svid_tg='27.11.'
+
