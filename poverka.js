619135c57a51fd77be355ee5c5639b1858e3a58a
ilgizDashkin <48960359+ilgizDashkin@users.noreply.github.com>
2021-08-04 08:02:19 +0500

    сделал сек ш ПС

--- poverka.js
+++ poverka.js
@@ -6,29 +6,29 @@ function date_year() {
 }
 // Передвижная высоковольтная лаборатория
 let nomer_svid_lab='8/81'
-let data_svid_lab='12.11.'
+let data_svid_lab='12.11.22'
 // Передвижная высоковольтная лаборатория SEBA для СПЭ
 let nomer_svid_lab_seba='8/195'
-let data_svid_lab_seba='12.11.'
+let data_svid_lab_seba='12.11.22'
 // Мегаомметр
 let nomer_svid_meg='8/1507'
-let data_svid_meg='27.10.'
+let data_svid_meg='27.10.22'
 // Психрометр
 let nomer_svid_psih='9/3695'
-let data_svid_psih='28.10.'
+let data_svid_psih='28.10.22'
 // барометр
 let nomer_svid_bar='9/3120'
-let data_svid_bar='26.10.'
+let data_svid_bar='26.10.22'
 // термометр
 let nomer_svid_term='9/1320'
-let data_svid_term='26.10.'
+let data_svid_term='26.10.22'
 // ПФИ
 let nomer_svid_pfi='8/5972'
-let data_svid_pfi='12.11.'
+let data_svid_pfi='12.11.22'
 // коэфиц-3
 let nomer_svid_ktr='8/5992'
-let data_svid_ktr='12.11.'
+let data_svid_ktr='12.11.22'
 // тангенс
 let nomer_svid_tg='8/4740'
-let data_svid_tg='27.11.'
+let data_svid_tg='27.11.22'
 
